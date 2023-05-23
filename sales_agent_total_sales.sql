d44e007b8b4267010a2e2f0f6afd8eb1b580e0e6
Nicholas Adams <njadams.722@gmail.com>
2023-05-23 16:09:04 -0500

    18

---
+++ sales_agent_total_sales.sql
@@ -0,0 +1,5 @@
+-- Provide a query that shows total sales made by each sales agent. The resultant table should include:
+-- Employee full name
+-- Total sales for each employee (all time)
+
+SELECT Employee.FirstName, Employee.LastName 
